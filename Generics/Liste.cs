639d369fea6052841a8cb360626c0d5a62eb173f
Fikret Gözütok <fikret_gztk@hotmail.com>
2021-01-21 00:08:41 +0300

    CSharpCourse

---
+++ Generics/Liste.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class Liste<T>
+    {
+        T[] array;
+
+        public Liste()
+        {
+            array = new T[0];
+        }
+        public void ekle(T eleman)
+        {
+            T[] tempArray = new T[(array.Length)+1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            tempArray[array.Length] = eleman;
+            array = tempArray;
+            
+        }
+
+        public int elemanSayisi()
+        {
+            return array.Length;
+        }
+
+        public T getir(int i)
+        {
+            return array[i];
+        }
+
+        public void sil(int index)
+        {
+            T[] tempArray = new T[(array.Length)-1];
+            T deletedData = getir(index);
+            int i = 0;
+            foreach (T item in array)
+            {
+                if (item.Equals(deletedData))
+                {
+                    
+                    continue;
+                }
+                else
+                {
+                    tempArray[i] = item;
+                    i++;
+                }
+            }
+            array = tempArray;
+        }
+
+        public void guncelle(T exItem, T newItem)
+        {
+            for(int i = 0; i<array.Length; i++)
+            {
+                if (array[i].Equals(exItem))
+                {
+                    array[i] = newItem;
+                }
+            }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bu projede generic yapısını kullanarak kendi liste yapımızı oluşturacağız.
+            Liste<string> myList = new Liste<string>();
+            Console.WriteLine(myList.elemanSayisi());
+            myList.ekle("fikret");
+            myList.ekle("ahmet");
+            myList.ekle("ismail");
+            myList.ekle("veli");
+            myList.ekle("gökhan");
+            myList.ekle("asma");
+            Console.WriteLine(myList.elemanSayisi());
+
+            for (int i = 0; i< myList.elemanSayisi(); i++)
+            {
+                Console.WriteLine(myList.getir(i));
+            }
+
+            myList.sil(2);
+
+            for (int i = 0; i < myList.elemanSayisi(); i++)
+            {
+                Console.WriteLine(myList.getir(i));
+            }
+
+            Console.WriteLine(myList.elemanSayisi());
+
+            myList.guncelle("fikret","tunahan");
+            for (int i = 0; i < myList.elemanSayisi(); i++)
+            {
+                Console.WriteLine(myList.getir(i));
+            }
+            Console.Read();
+        }
+    }
+}
